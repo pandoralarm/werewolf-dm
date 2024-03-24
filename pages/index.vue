55c3aa55d5de89526931d3def73757d78979d914
pandoralarm <08889237023a@gmail.com>
2024-03-24 16:33:48 +0700

    game start:

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,22 @@
 <template>
-    <div class="main-wrapper" :class="condition.time === 'day' ? 'day' : 'night'">
+    <div class="main-wrapper" :class="[condition.time === 'day' ? 'day' : 'night', { compact: isGameRunning }]">
         <div class="banner" :class="{ compact: isGameRunning }">
             <div class="title">The Werewolf Game</div>
             <div class="progress animate">{{ condition.progressText }}</div>
             <div class="start-button" :class="{ downloading: isDownloading }" @click="runGame()"><span v-if="!isGameRunning">Mulai</span></div>
         </div>
+
+        <div class="card" :class="{ disabled: !isGameRunning, enabled: isGameRunning }">
+            <div class="card-head">
+                <img :src="condition.card.head" />
+            </div>
+
+            <div class="card-content">
+                <div class="title">{{ condition.card.title }}</div>
+                <div class="description">{{ condition.card.description }}</div>
+            </div>
+        </div>
+
         <audio v-for="source in sfx_sources" :id="source.id" :loop="source.loop">
             <source :src="source.src" type="audio/mpeg" />
             Your browser does not support the audio element.
@@ -20,6 +32,11 @@
                 condition: {
                     time: "night",
                     progressText: "Memindai Data",
+                    card: {
+                        head: "/cards/roles/gm.png",
+                        title: "Title",
+                        description: "Lorem Ipsum",
+                    },
                 },
                 sfx_sources: [
                     {
@@ -65,15 +82,15 @@
         methods: {
             runGame() {
                 this.isGameRunning = true;
-
+                this.condition.progressText = "Memulai Permainan";
                 this.playAudio("click");
                 this.playAudio("day_ominous");
             },
             init() {
-                const hasRunBefore = localStorage.getItem("game_condition");
+                const hasRunBefore = localStorage.getItem("has_played");
                 // const hasRunBefore = false;
                 if (!hasRunBefore) {
-                    localStorage.setItem("game_condition", JSON.stringify(this.condition));
+                    localStorage.setItem("has_played", true);
 
                     setTimeout(() => {
                         this.condition.progressText = "Mengunduh Data";
@@ -86,7 +103,7 @@
                     }, 10000);
                 } else {
                     setTimeout(() => {
-                        this.condition = JSON.parse(hasRunBefore);
+                        // this.condition = JSON.parse(hasRunBefore);
                         this.condition.progressText = "";
                         this.isDownloading = false;
                     }, 1000);
@@ -116,14 +133,75 @@
 </script>
 
 <style lang="scss">
+    .card {
+        border: 2px solid #fff;
+        height: 80%;
+        width: 85%;
+        padding: 12px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        border-radius: 12px;
+
+        .card-head {
+            display: flex;
+            justify-content: center;
+            background-color: #999999;
+            width: 100%;
+            height: 25%;
+            padding: 6px;
+            img {
+                max-width: 50%;
+                object-fit: contain;
+            }
+        }
+
+        .card-content {
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding: 22px;
+
+            .title {
+                font-size: 6vw;
+                font-family: "BoltonBold", sans-serif;
+            }
+        }
+    }
+    .disabled {
+        user-select: none;
+        pointer-events: none;
+        opacity: 0;
+        max-height: 0;
+        transition: all 2s cubic-bezier(0.075, 0.82, 0.165, 1);
+    }
+
+    .enabled {
+        user-select: all;
+        pointer-events: all;
+        opacity: 1;
+        max-height: 100%;
+        transition: all 2s cubic-bezier(0.075, 0.82, 0.165, 1);
+    }
+
+    div {
+        transition: all 2s cubic-bezier(0.075, 0.82, 0.165, 1);
+    }
     .main-wrapper {
         display: flex;
         width: 100vw;
         height: 100vh;
         justify-content: center;
+        flex-direction: column;
+        align-items: center;
         transition: all 1.5s cubic-bezier(0.075, 0.82, 0.165, 1);
         // Default background
         background-color: #121941;
+
+        &.compact {
+            justify-content: flex-start;
+        }
 
         .banner {
             padding: 2vh 0;
@@ -132,6 +210,7 @@
             align-items: center;
             flex-direction: column;
             gap: 22px;
+            text-align: center;
 
             max-height: 100vh;
             transition: all 2s cubic-bezier(0.25, 0.46, 0.45, 0.94);
@@ -141,6 +220,7 @@
 
                 .title {
                     transform: scale(0.5) translateY(-1.5vh);
+                    max-height: 50%;
                 }
 
                 .progress {
